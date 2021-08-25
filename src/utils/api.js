6f11f97342503d095a339ff84a2a90c187f92266
Selman Samet <selmansametun@gmail.com>
2021-08-26 01:46:12 +0300

    BASE_URL changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,36 +1,36 @@
 const fetch = require('node-fetch');
 
 const BASE_URL = process.env.NODE_ENV !== "production"
-	? "3.120.152.26:8080"
-	: "127.0.0.1:8080"
+	? "https://api.moot.to"
+	: "http://127.0.0.1:8080"
 
 const getMe = () => {
-	return fetch(`http://${BASE_URL}/me`, { credentials: 'include' })
+	return fetch(`${BASE_URL}/me`, { credentials: 'include' })
 		.then(res => res.json())
 }
 
 const getStatus = (id) => {
-	return fetch(`http://${BASE_URL}/status/${id}`, { credentials: 'include' })
+	return fetch(`${BASE_URL}/status/${id}`, { credentials: 'include' })
 		.then(res => res.json())
 }
 
 const getTree = (id) => {
-	return fetch(`http://${BASE_URL}/tree/${id}`, { credentials: 'include' })
+	return fetch(`${BASE_URL}/tree/${id}`, { credentials: 'include' })
 		.then(res => res.json())
 }
 
 const sendTweet = (params) => {
-	const url = `http://${BASE_URL}/tweet?status=${params.text}&repliedTo=${params.to}&type=${params.type}`
+	const url = `${BASE_URL}/tweet?status=${params.text}&repliedTo=${params.to}&type=${params.type}`
 	return fetch(url, { credentials: 'include' }).then(res => res.json())
 }
 
 const likeTweet = (id) => {
-	const url = `http://${BASE_URL}/like/${id}`
+	const url = `${BASE_URL}/like/${id}`
 	return fetch(url, { credentials: 'include' }).then(res => res.json())
 }
 
 const logout = () => {
-	const url = `http://${BASE_URL}/logout`
+	const url = `${BASE_URL}/logout`
 	return fetch(url, { credentials: 'include' })
 		.then(res => res.json())
 		.then(res => window.location.reload())
